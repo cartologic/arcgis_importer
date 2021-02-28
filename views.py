89a9b78dc760063514aa2c81311e2923adedcb62
Ahmed Nour Eldeen <ahmednoreldeen@gmail.com>
2021-02-28 08:34:43 +0200

    set app access to logged in users only

--- views.py
+++ views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from . import APP_NAME
@@ -8,6 +9,7 @@ from . import APP_NAME
 # Create your views here.
 
 
+@login_required
 def index(request):
     context = {"APP_NAME": APP_NAME}
     return render(request, template_name="{}/index.html".format(APP_NAME),
